0c296dd000c7c52b99a6644f3d901f6673732a8b
ashfaque9426 <ashfaqsylvi9426@gmail.com>
2023-05-04 12:51:14 +0600

    adding my public server repo for the food artisan

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5100;
+const cors = require('cors');
+const data = require('./data/data.json');
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('The Food Artisan server is running');
+});
+
+app.get('/chefData', (req, res) => {
+    res.send(data);
+});
+
+app.get('/recipes/:id', (req, res) => {
+    const id = req.params.id;
+    const recepes = data.find(chefData => chefData.id === id);
+    res.send(recepes);
+})
+
+app.listen(port, () => {
+    console.log(`The Food Artisan server is runnig on port: ${port}`)
+});
